765f97a614c4be3603813ec7a90aadab81853d8a
jose carlos <jose carlos@LAPTOP-TS9I0MFL>
2022-06-29 18:27:03 -0500

    primera subida del proyecto

--- H4github/src/h4github/H4github.java
+++ H4github/src/h4github/H4github.java
@@ -7,6 +7,10 @@ public class H4github {
    
     public static void main(String[] args) {
         // TODO code application logic here
+        int x =4;
+        int y =5;
+        int suma = x+y;
+        System.out.println("la suma es:"+suma);
     }
     
 }
